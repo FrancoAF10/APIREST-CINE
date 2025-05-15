69415b92b5bf725557899f3bfca0e5faa995b1bb
Gian Franco Anton Felix <antonfelixfranco@gmail.com>
2025-05-15 08:24:15 -0500

    Mejoras CRUD

--- src/controllers/peliculas.controller.js
+++ src/controllers/peliculas.controller.js
@@ -3,23 +3,35 @@ import {pool} from "../db.js"
 
 //Lógica (backend) de cada enpoint
 export const getPeliculas=async(req,res)=>{
-  const [rows]=await pool.query("SELECT * FROM PELICULAS")
+  try{
+    const [rows]=await pool.query("SELECT * FROM PELICULAS")
   res.json(rows)
+  }catch(error){
+return res.status(500).json({
+  message: 'No se concretó la consulta'
+})
+}
 }
 
 export const getPeliculaById=async(req,res)=>{
-  const [rows]=await pool.query("SELECT * FROM PELICULAS WHERE id=?",[req.params.id])
+  try{
+    const [rows]=await pool.query("SELECT * FROM PELICULAS WHERE id=?",[req.params.id])
   if(rows.length<=0){
     return res.status(404).json({
       message:'No existe pelicula con ese ID'
     })
   }
+}catch(error){
+return res.status(500).json({
+  message: 'No se concretó la consulta'
+})
+}
   res.json(rows)
   
 }
 
 export const createPeliculas= async(req,res)=>{
-  //1. Obtener datos del Json (input)
+try{  //1. Obtener datos del Json (input)
   const {titulo,duracion,clasificacion,alanzamiento}=req.body
 // 2. Ejecutar la consulta, pasa valores obtenidos
   const [rows]=await pool.query("INSERT INTO PELICULAS (titulo,duracion,clasificacion,alanzamiento)VALUES(?,?,?,?)",
@@ -32,10 +44,15 @@ export const createPeliculas= async(req,res)=>{
     clasificacion,
     alanzamiento
   })
+}catch(error){
+  return res.status(500).json({
+  message: 'No se concretó la consulta'
+})
 }
-
+}
 export const updatePeliculas=async(req,res)=>{
-  const id=req.params.id
+  try{
+    const id=req.params.id
   const {titulo,duracion,clasificacion,alanzamiento}=req.body
 
   const querySQL= `
@@ -57,10 +74,16 @@ export const updatePeliculas=async(req,res)=>{
   res.json({
     message:"Actualización Correcta"
   })
+}catch(error){
+  return res.status(500).json({
+  message: 'No se concretó la consulta'
+})
+}
 }
 
 export const deletePeliculas=async(req,res)=> {
-  const [result]=await pool.query("DELETE FROM PELICULAS WHERE id=?",[req.params.id])
+ try{
+   const [result]=await pool.query("DELETE FROM PELICULAS WHERE id=?",[req.params.id])
  if(result.affectedRows<=0){
   return res.status(404).json({
     message:"No existe registro con este ID"
@@ -68,4 +91,9 @@ export const deletePeliculas=async(req,res)=> {
  }
  //¿Y si borra correctamente?
  res.sendStatus(204)
+}catch(error){
+  return res.status(500).json({
+  message: 'No se concretó la consulta'
+})
+}
 }
